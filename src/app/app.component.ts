cb1cc2becc7333f00f961f2c7684b1530cf2242e
Namito Yokota <namitoyokota@icloud.com>
2024-07-09 20:54:12 -0500

    Added sound effect.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,31 @@ export class AppComponent {
 
     sentMessages: string[] = [];
 
+    @ViewChild('sentSound') sentSound: ElementRef;
+
     @ViewChild('newMessageInput', { static: false }) newMessageInput: ElementRef;
 
     constructor() {}
 
     sendMessage(): void {
+        if (!this.newMessage) {
+            return;
+        }
+
         this.sentMessages.push(this.newMessage);
         this.newMessage = '';
+        this.playSound();
     }
 
     focusBackToInput(): void {
         this.newMessageInput.nativeElement.focus();
+    }
+
+    private playSound(): void {
+        if (this.sentSound.nativeElement.paused) {
+            this.sentSound.nativeElement.play();
+        } else {
+            this.sentSound.nativeElement.currentTime = 0;
+        }
     }
 }
