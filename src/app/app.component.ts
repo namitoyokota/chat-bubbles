1768b191322c274beaa57101dcd2825dc7e9995f
Namito Yokota <namitoyokota@icloud.com>
2024-07-09 21:56:10 -0500

    Added iMessage styling to sent messages.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,7 +38,7 @@ export class AppComponent implements OnInit {
         this.newMessage = '';
         this.playSound();
 
-        asyncScheduler.schedule(() => this.removeMessage.next(), 5000);
+        asyncScheduler.schedule(() => this.removeMessage.next(), 8000);
     }
 
     focusBackToInput(): void {
