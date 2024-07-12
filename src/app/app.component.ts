b3513a9b9461edbadc49ff52491029ab78969e23
Namito Yokota <namitoyokota@icloud.com>
2024-07-12 16:46:06 -0500

    Removed unnecessary animation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,13 +16,9 @@ class Message {
     animations: [
         trigger('list', [transition(':enter', [query('@items', stagger(100, animateChild()))])]),
         trigger('items', [
-            transition(':enter', [
-                style({ transform: 'scale(0.5)', opacity: 0 }),
-                animate('1s cubic-bezier(.8,-0.6,0.2,1.5)', style({ transform: 'scale(1)', opacity: 1 })),
-            ]),
             transition(':leave', [
                 style({ transform: 'scale(1)', opacity: 1, height: '*' }),
-                animate('1s cubic-bezier(.8,-0.6,0.2,1.5)', style({ transform: 'scale(0.8)', opacity: 0, height: '0px', margin: '0px' })),
+                animate('1s cubic-bezier(.8,-0.6,0.2,1.5)', style({ transform: 'scale(0.8)', opacity: 0, height: '0px' })),
             ]),
         ]),
     ],
@@ -51,7 +47,7 @@ export class AppComponent implements OnInit {
             return;
         }
 
-        this.messages = [new Message(crypto.randomUUID(), ''), ...this.messages];
+        this.messages.unshift(new Message(crypto.randomUUID(), ''));
         this.playSound();
         asyncScheduler.schedule(() => this.removeMessage.next(), 8000);
     }
